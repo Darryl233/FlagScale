166a3c70092966a3ee9d11a254f3fe80a74923b6
liyuzhuo <lee.yuzhuo233@gmail.com>
2026-05-20 14:12:30 +0800

    fix

--- tests/test_utils/runners/setup_unit_test_env.sh
+++ tests/test_utils/runners/setup_unit_test_env.sh
@@ -97,7 +97,8 @@ setup_metax_unit_env() {
     pip install . --no-build-isolation 
 
     git clone --depth 1 https://github.com/flagos-ai/TransformerEngine-FL.git
-    TE_FL_SKIP_CUDA=1 python -m pip install /workspace/TransformerEngine-FL --no-build-isolation
+    cd TransformerEngine-FL
+    TE_FL_SKIP_CUDA=1 python -m pip install TransformerEngine-FL --no-build-isolation
 }
 
 setup_ascend_unit_env() {
